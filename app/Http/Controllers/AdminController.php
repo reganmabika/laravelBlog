5411717679ad9b16dfba069e1cb765fa5613ed0a
Regan M <44497470+reganmabika@users.noreply.github.com>
2021-10-01 09:51:32 +0200

    Added auth controllers

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        $user = Auth::user();
+
+        //dd($user->role);
+
+        return view('admin');
+
+
+
+    }
+}
